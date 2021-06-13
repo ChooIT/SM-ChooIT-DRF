58f892545701a4b18449f550f3978084b5ff6b9b
shunnyjang <suuuu101@naver.com>
2021-06-14 03:34:27 +0900

    Add Accounts Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from accounts.models import User, UserTag, Tag, Nickname
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'nickname', 'type', 'is_active', 'gender']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['tag_code', 'tag_text']
+
+
+@admin.register(UserTag)
+class UserTagAdmin(admin.ModelAdmin):
+    list_display = ['user', 'tag', 'created_at']
+
+
+@admin.register(Nickname)
+class NicknameAdmin(admin.ModelAdmin):
+    list_display = ['adj', 'noun', 'number']
