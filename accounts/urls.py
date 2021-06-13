51aeca11cf3d7c6881066d33379fa6ee36fac47d
shunnyjang <suuuu101@naver.com>
2021-06-14 03:50:43 +0900

    Add login routers

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .API import signup
+from .API import signup, login
 
 
 urlpatterns = [
     path('email/', signup.is_registered_email),
     path('signup/', signup.create_user),
-    path('tag/', signup.create_new_favorite_tag)
+    path('tag/', signup.create_new_favorite_tag),
+    path('login/', login.LoginView.as_view()),
 ]
