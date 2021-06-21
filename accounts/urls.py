01d2d4cee8a7a37b47a2da3d648b293207b66ca9
shunnyjang <suuuu101@naver.com>
2021-06-21 18:14:10 +0900

    Add url path of last auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,8 @@ from rest_framework_jwt.views import ObtainJSONWebToken
 urlpatterns = [
     path('email/', signup.is_registered_email),
     path('signup/', signup.create_user),
-    path('tag/', signup.create_new_favorite_tag),
-    path('login/', ObtainJSONWebToken.as_view()),
+    path('signup/tag/', signup.create_new_favorite_tag),
+    path('signup/product/', signup.create_using_prod_history),
+    path('login/', profile.LoginView.as_view()),
     path('detail/', profile.get_profile_detail),
 ]
