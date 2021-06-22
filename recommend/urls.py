bdc9ece995630147bc68c03f621b9d5f20dd06b1
shunnyjang <suuuu101@naver.com>
2021-06-22 14:23:54 +0900

    Feat: list category, tag

--- recommend/urls.py
+++ recommend/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import product, review
+from .views import product, review, data
 
 urlpatterns = [
-    path('', product.ProductViewset.as_view({"get": "list"}), name="product-list"),
-    path('<int:pk>/', product.ProductViewset.as_view({"get": "retrieve"}, name="product-retrieve")),
+    path('', product.tag_filtering_product_list),
+    path('<int:pk>/', product.get_product_detail),
     path('favorite/', product.create_user_favorite_product),
     path('image/', review.post_review_image),
     path('review/', review.review_list),
     path('review/post/', review.post_new_review),
     path('review/<int:pk>/', review.review_retrieve),
+    path('category/', data.CategoryList.as_view()),
+    path('tag/', data.TagList.as_view()),
 ]
