eb5fdd20a8098506dee0d91411da38412c072c79
shunnyjang <suuuu101@naver.com>
2021-08-30 23:14:19 +0900

    Fix: FieldError

--- recommend/models.py
+++ recommend/models.py
@@ -44,7 +44,7 @@ class Product(models.Model):
         return "%d. [%s]%s" % (self.prod_no, self.prod_manufacturer, self.prod_name)
 
     class Meta:
-        ordering = ['id']
+        ordering = ['prod_no']
 
 
 class ProductImage(models.Model):
