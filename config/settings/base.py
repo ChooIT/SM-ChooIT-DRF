c2ce789ec0bcd53b799e630c93e235ba76f0ddaf
shunnyjang <suuuu101@naver.com>
2021-09-08 12:39:07 +0900

    Add graphic config
    python manage.py graph_models -a > example.dot
    python manage.py graph_models -a -g -o exmample_visualized.png

--- config/settings/base.py
+++ config/settings/base.py
@@ -202,3 +202,8 @@ CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = TIME_ZONE
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers.DatabaseScheduler"
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
